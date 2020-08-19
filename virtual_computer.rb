8ccb023eae7410896741e55f3fe5780e6745922d
nima-19 <nima.thakare@gmail.com>
2020-08-19 10:29:48 +0530

    Virtual Computer

---
+++ virtual_computer.rb
@@ -0,0 +1,20 @@
+class Computer
+ 	@@users = {}
+ 	def initialize(username,password)
+ 		@username = username
+ 		@password = password
+ 		@files = {}
+ 		@@users[username] = password
+ 	end
+
+  	def create(filename)
+    		time = Time.now
+    		@files = {:filename => time}
+    		puts "#{filename} was created by #{@username} at #{time}."
+  	end
+
+  	def Computer.get_users
+    		@@users
+  	end
+end
+puts my_computer = Computer.new("nima",1234)
